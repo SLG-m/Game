d54ed46a3b8618cbe19889a6176b429cb5210b5e
Jesus <glokhl@mail.ru>
2025-04-24 01:21:48 +0300

    сделан паралакс, начальная анимация(до 9 видео)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,44 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 5f;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    [SerializeField ]
+    private bool _isMoving = false;
+
+    public bool IsMoving 
+    { get 
+        {
+        return _isMoving;
+        } 
+        private set 
+        { 
+            _isMoving = value;
+            animator.SetBool("isMoving", value); 
+        }
+    }
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight { get 
+        { 
+            return _isFacingRight;
+        } 
+        private set 
+        { 
+           if (_isFacingRight != value)
+           {
+                transform.localScale *= new Vector2(-1, 1);
+           }
+
+        _isFacingRight= value;
+        } }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -38,6 +69,20 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        } 
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;    
+        }
     }
 }
  
