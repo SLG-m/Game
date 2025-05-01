b9ee1eae243145c2f59786b8e2807dbfded8c16c
Jesus <glokhl@mail.ru>
2025-05-01 05:15:30 +0300

    сделал уровень, сделал в гравном меню панель управления

--- Assets/Scripts/TouchingDirections.cs
+++ Assets/Scripts/TouchingDirections.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class TouchingDirections : MonoBehaviour
 {
-    //Rigidbody2D rb;
     public ContactFilter2D castFilter;
     public float groundDistance = 0.05f;
     public float wallDistance = 0.2f;
     public float ceilingDistance = 0.05f;
-
 
     CapsuleCollider2D touchingCol;
     Animator animator;
@@ -19,18 +17,17 @@ public class TouchingDirections : MonoBehaviour
     [SerializeField]
     private bool _isGrounded;
 
-    public bool IsGrounded 
-    { 
-        get 
-        { 
-        return _isGrounded;
-        } 
-        private set 
-        { 
-        _isGrounded = value;
-
-        animator.SetBool(AnimationStrings.isGrounded, value);
-        } 
+    public bool IsGrounded
+    {
+        get
+        {
+            return _isGrounded;
+        }
+        private set
+        {
+            _isGrounded = value;
+            animator.SetBool(AnimationStrings.isGrounded, value);
+        }
     }
 
     [SerializeField]
@@ -38,22 +35,19 @@ public class TouchingDirections : MonoBehaviour
 
     public bool IsOnWall
     {
-        get 
+        get
         {
             return _isOnWall;
         }
         private set
         {
             _isOnWall = value;
-
             animator.SetBool(AnimationStrings.isOnWall, value);
         }
     }
 
     [SerializeField]
     private bool _isOnCeiling;
-
-    private Vector2 wallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
 
     public bool IsOnCeiling
     {
@@ -64,21 +58,25 @@ public class TouchingDirections : MonoBehaviour
         private set
         {
             _isOnCeiling = value;
-
             animator.SetBool(AnimationStrings.isOnCeiling, value);
         }
     }
+
     private void Awake()
     {
-        // rb = GetComponent<Rigidbody2D>();
         touchingCol = GetComponent<CapsuleCollider2D>();
         animator = GetComponent<Animator>();
+    }
+
+    private Vector2 GetWallCheckDirection()
+    {
+        return gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
     }
 
     void FixedUpdate()
     {
         IsGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
-        IsOnWall = touchingCol.Cast(wallCheckDirection, castFilter, wallHits, wallDistance) > 0;
+        IsOnWall = touchingCol.Cast(GetWallCheckDirection(), castFilter, wallHits, wallDistance) > 0;
         IsOnCeiling = touchingCol.Cast(Vector2.up, castFilter, ceilingHits, ceilingDistance) > 0;
     }
 }
